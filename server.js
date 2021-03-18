8fa51b7d21b2701a3af0c1b7b91567c6a9d3d9a9
isaacaaa <54743965+isaacaaa@users.noreply.github.com>
2021-03-18 23:53:09 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+const HOST = '127.0.0.1';
+const PORT = 20213;
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${rinfo.port}:${msg} `);
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+
+
+
+
+
+server.bind({
+    address: HOST,
+    port: PORT,
+    exclusive: true
+});
