6fd02d930711396ca1a21c15e8c19397a098cabf
isaacaaa <54743965+isaacaaa@users.noreply.github.com>
2021-04-02 11:52:04 +0800

    Add files via upload

---
+++ lab-remoting/grpc/client.js
@@ -0,0 +1,29 @@
+var PROTO_PATH = __dirname + '/helloworld.proto';
+
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+function main() {
+    var client = new hello_proto.Greeter('localhost:50051',
+        grpc.credentials.createInsecure());
+
+    client.sayHello({name: 'Tom'}, function (err, response) {
+        console.log('Greeting:', response.message);
+    });
+    client.add({x: 1, y:2}, function (err, respone) {
+    	console.log('add:',respone.message);
+    });
+
+}
+
+main();
---
+++ lab-remoting/soap/soapServer.js
@@ -0,0 +1,26 @@
+const soap = require('soap');
+const http = require('http');
+//const xml = require('fs').readFileSync('Adder.wsdl', 'utf8');
+const xml = require('fs').readFileSync('AddMul.wsdl', 'utf8');
+const service = {
+    CalculatorImplService: {
+        CalculatorImplPort: {
+            add: function (args) {
+                return {result: args.x + args.y};
+            },
+	    multiply: function (args) {
+		return {result: args.x * args.y};
+	    }
+        }
+    }
+};
+
+const server = http.createServer(function (request, response) {
+    response.end('404: Not Found: ' + request.url);
+});
+
+soap.listen(server, '/AddMul', service, xml, function () {
+    console.log('server initialized');
+});
+
+server.listen(8192);
